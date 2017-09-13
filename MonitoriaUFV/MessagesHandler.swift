4952894a0f2d87edc2cb40c30cfddbb4f21ddb84
Daniel Araujo <danielaraujos@live.com>
2017-09-12 21:29:27 -0300

    Começando a criar o chat

---
+++ MonitoriaUFV/MessagesHandler.swift
@@ -0,0 +1,15 @@
+//
+//  MessagesHandler.swift
+//  MonitoriaUFV
+//
+//  Created by Daniel Araújo on 12/09/17.
+//  Copyright © 2017 Daniel Araújo Silva. All rights reserved.
+//
+
+import Foundation
+
+
+class MessagesHandler {
+
+
+}
