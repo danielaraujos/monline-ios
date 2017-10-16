9169fd0eacde4605f75f9468dfbd03cc69bd22f1
Daniel Araujo <danielaraujos@live.com>
2017-10-16 00:03:06 -0200

    Otimizando o código de listagem das monitorias. Agora mostrará todas as monitorias que ele não é monitor.

--- MonitoriaUFV/MonitoringVC.swift
+++ MonitoriaUFV/MonitoringVC.swift
@@ -26,7 +26,6 @@ class MonitoringVC: UIViewController, UITableViewDataSource, UITableViewDelegate
         super.viewDidLoad()
         self.back()
         SVProgressHUD.show(withStatus: "Carregando")
-        self.buscarMonitoriaQueSouMonitor()
         self.buscarCursos()
         
     }
@@ -40,53 +39,72 @@ class MonitoringVC: UIViewController, UITableViewDataSource, UITableViewDelegate
         backItem.title = " "
         navigationItem.backBarButtonItem = backItem
     }
+//
+//    func buscarMonitoriaQueSouMonitor() {
+//        let ref = Database.database().reference().child(Constantes.MONITORIAS)
+//        ref.observe(.childAdded, with: { (snapshot) in
+//            let nomeDisciplina = snapshot.key as! String
+//            let cursoUsuarioRef = Database.database().reference().child(Constantes.MONITORIAS).child(nomeDisciplina)
+//            cursoUsuarioRef.observeSingleEvent(of: .value, with: { (conteudo) in
+//                if let dictionary = conteudo.value as? [String: AnyObject] {
+//                    let novaMonitorias = Monitoria(dictionary: dictionary)
+//                    if(novaMonitorias.monitor! == self.meuID){
+//                        self.disciplinaMonitor = nomeDisciplina
+//                    }
+//                }
+//            }, withCancel: nil)
+//        }, withCancel: nil)
+//    }
     
-    func buscarMonitoriaQueSouMonitor() {
-        let ref = Database.database().reference().child(Constantes.MONITORIAS)
-        ref.observe(.childAdded, with: { (snapshot) in
-            let nomeDisciplina = snapshot.key as! String
-            let cursoUsuarioRef = Database.database().reference().child(Constantes.MONITORIAS).child(nomeDisciplina)
-            cursoUsuarioRef.observeSingleEvent(of: .value, with: { (conteudo) in
-                if let dictionary = conteudo.value as? [String: AnyObject] {
-                    let novaMonitorias = Monitoria(dictionary: dictionary)
-                    if(novaMonitorias.monitor! == self.meuID){
-                        self.disciplinaMonitor = nomeDisciplina
-                    }
-                }
-            }, withCancel: nil)
-        }, withCancel: nil)
-    }
-
+    /*
+     Leio a estrutura Cursos, leio a estrutura dos usuario e verifico na lista de usuarios, qual é o meu usuario
+     Assim passo por paramentro para proxima função a sigla
+     */
     func buscarCursos() {
         let ref = Database.database().reference().child(Constantes.CURSOS)
         ref.observe(.childAdded, with: { (snapshot) in
-            let sigla = snapshot.key
+            let siglaCurso = snapshot.key
             let cursoUsuarioRef = Database.database().reference().child(Constantes.USUARIOS)
             cursoUsuarioRef.observe(.childAdded, with: { (conteudo) in
                 let idUsuarios = conteudo.key
                 if self.meuID == idUsuarios{
-                    self.buscarCursoUsuario(self.meuID, sigla)
+                    self.buscarCursoUsuario(siglaCurso)
                 }
             }, withCancel: nil)
+            
         }, withCancel: nil)
     }
-    
-    fileprivate func buscarCursoUsuario(_ id: String, _ sigla:String) {
-        let conteudoReferencia = Database.database().reference().child(Constantes.USUARIOS).child(id).child(Constantes.CURSO)
+
+    /*
+     Leio agora o nome da que esta no atributo sigla.
+     Este nome é a sigla do curso que ele esta cursando
+     */
+    fileprivate func buscarCursoUsuario(_ sigla:String) {
+        let conteudoReferencia = Database.database().reference().child(Constantes.USUARIOS).child(self.meuID)
         conteudoReferencia.observeSingleEvent(of: .value, with: { (snapshot) in
-            let curso = snapshot.value as! String
-            if(curso == sigla){
-                self.buscarAsMonitoriasDoCurso(sigla)
+            if let dictionary = snapshot.value as? [String: AnyObject] {
+                var usuario = Usuario(dictionary: dictionary)
+                var cursoQueFaco = usuario.curso!
+                if(cursoQueFaco == sigla){
+                    self.buscarAsMonitoriasDoCurso(sigla, usuario.monitor! )
+                }
             }
+
         }, withCancel: nil)
     }
     
-    fileprivate func buscarAsMonitoriasDoCurso(_ sigla:String) {
+    
+    /*
+     Leio agora a estrutura cursos, na sigla que o usuario atual esta, e no atributo tipo.
+     Pegando o nome da disciplina e a sigla da disciplina.
+     */
+    
+    fileprivate func buscarAsMonitoriasDoCurso(_ sigla:String, _ disciplinaMonitor:String) {
         let cursoRef = Database.database().reference().child(Constantes.CURSOS).child(sigla).child(Constantes.TIPO)
         cursoRef.observeSingleEvent(of: .value, with: { (filho) in
             if let dictionary = filho.value as? [String: AnyObject] {
                 for(key , value ) in dictionary {
-                    if(key != self.disciplinaMonitor){
+                    if(key != disciplinaMonitor){
                         let novaMonitorias = Curso(nome: value as! String, sigla: key as! String)
                         self.monitorias.append(novaMonitorias)
                     }
@@ -98,7 +116,6 @@ class MonitoringVC: UIViewController, UITableViewDataSource, UITableViewDelegate
             })
         }, withCancel: nil)
     }
-    
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1;
